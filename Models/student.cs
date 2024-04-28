c0bd78b65cd194eb93562035fa6d5c5c1c18bea7
Subha9932 <bossubhadip19@gmail.com>
2024-04-28 18:58:28 +0530

    first commit

---
+++ Models/student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace newPo.Models
+{
+	public class student
+	{
+        [StringLength(2)]
+		public string? name { get; set; }
+        public int age { get; set; }
+        public int reg { get; set; }
+        [EmailAddress]
+        public string? home { get; set; }
+        public Gender gender { get; set; }
+
+    }
+    public enum Gender
+    {
+        male,female
+
+    }
+}
+
